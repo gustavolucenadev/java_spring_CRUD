86b91cca06d0d426f2f48f92b4cea56d75c46597
gustavolucenadev <gus_duarte8@hotmail.com>
2025-02-16 11:32:06 -0300

    feat: "adding pageable to get a paginated search"

--- src/main/java/com/challenge/crud/crud/controllers/ClientController.java
+++ src/main/java/com/challenge/crud/crud/controllers/ClientController.java
@@ -4,11 +4,14 @@ import com.challenge.crud.crud.dto.ClientDTO;
 import com.challenge.crud.crud.entities.Client;
 import com.challenge.crud.crud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -30,5 +33,10 @@ public class ClientController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}")
                 .buildAndExpand(dto.getID()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return clientService.findAll(pageable);
     }
 }
--- src/main/java/com/challenge/crud/crud/services/ClientService.java
+++ src/main/java/com/challenge/crud/crud/services/ClientService.java
@@ -6,8 +6,12 @@ import com.challenge.crud.crud.entities.Client;
 import com.challenge.crud.crud.repositories.ClientRepository;
 import com.challenge.crud.crud.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -22,6 +26,11 @@ public class ClientService {
     }
 
     // findAll - busca paginada
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = clientRepository.findAll(pageable);
+        return result.map(ClientDTO::new);
+    }
 
     // insert
     public ClientDTO insert(ClientDTO dto) {
